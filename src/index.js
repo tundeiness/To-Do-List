4c2ca40b1091d971f73245e0c548b0a200ea1496
tundeiness <tundeiness@yahoo.co.uk>
2019-11-20 16:14:56 +0100

    create new branch

--- src/index.js
+++ src/index.js
@@ -10,69 +10,60 @@ const newListInput = document.querySelector('[data-new-list-input]');
 const deleteListButton = document.querySelector('[data-delete-list-button]');
 
 const addingEntry = () => {
-
-
-
-  listsContainer.addEventListener('click', e => {
-    if(e.target.tagName.toLowerCase() === 'li'){
-      selectedListId = e.target.dataset.listId
+  listsContainer.addEventListener('click', (e) => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+      selectedListId = e.target.dataset.listId;
       save();
       todoList();
     }
   });
 
-  deleteListButton.addEventListener('click', e => {
-      lists = lists.filter(list => list.id !== selectedListId);
-      selectedListId = null;
-      save();
-      todoList();
+  deleteListButton.addEventListener('click', (e) => {
+    lists = lists.filter((list) => list.id !== selectedListId);
+    selectedListId = null;
+    save();
+    todoList();
   });
 
-  newListForm.addEventListener('submit', e =>{
+  newListForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const listName = newListInput.value;
-    if(listName == null || listName === '') return 
+    if (listName == null || listName === '') return;
     const list = createList(listName);
     newListInput.value = null;
     lists.push(list);
     save();
     todoList();
-});
-
-}
+  });
+};
 
 const todoList = () => {
-
-    clearElement(listsContainer)
-    lists.forEach(list => {
-        const listElement = document.createElement('li');
-        listElement.dataset.listId =list.id;
-        listElement.classList.add('list-group-item');
-        listElement.innerText = list.name;
-        if(list.id === selectedListId){
-            console.log(selectedListId);
-        listElement.classList.add('active');
-        }
-        listsContainer.appendChild(listElement);
-    })
-
-  
+  clearElement(listsContainer);
+  lists.forEach((list) => {
+    const listElement = document.createElement('li');
+    listElement.dataset.listId = list.id;
+    listElement.classList.add('list-group-item');
+    listElement.innerText = list.name;
+    if (list.id === selectedListId) {
+      console.log(selectedListId);
+      listElement.classList.add('active');
+    }
+    listsContainer.appendChild(listElement);
+  });
 };
 
 const save = () => {
-    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
-    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 };
 
-const clearElement = (element) =>{
-    while(element.firstChild){
-        element.removeChild(element.firstChild);
-    }
+const clearElement = (element) => {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
 };
 
-const createList = (name) => {
- return {id: Date.now().toString(), name: name, tasks: []}
-};
+const createList = (name) => ({id: Date.now().toString(), name: name, tasks: []});
 
 todoList();
 addingEntry();
