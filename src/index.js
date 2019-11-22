94854a4633be6f78b81c78b66fbeb1d726694521
tundeiness <tundeiness@yahoo.co.uk>
2019-11-22 17:38:23 +0100

    fix errors

--- src/index.js
+++ src/index.js
@@ -30,62 +30,6 @@ function createTask(name) {
   return { id: Date.now().toString(), name, complete: false };
 }
 
-const saveAndRender = () => {
-  save();
-  todoList();
-};
-
-  listsContainer.addEventListener('click', (e) => {
-    if (e.target.tagName.toLowerCase() === 'li') {
-      selectedListId = e.target.dataset.listId;
-      saveAndRender();
-    }
-  });
-
-  clearCompleteTasksButton.addEventListener('click', (e) => {
-    const selectedList = lists.find((list) => list.id === selectedListId);
-    selectedList.tasks = selectedList.tasks.filter((task) => !task.complete);
-    saveAndRender();
-  });
-
-  deleteListButton.addEventListener('click', (e) => {
-    lists = lists.filter((list) => list.id !== selectedListId);
-    selectedListId = null;
-    saveAndRender();
-  });
-
-  newListForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const listName = newListInput.value;
-    if (listName == null || listName === '') return;
-    const list = createList(listName);
-    newListInput.value = null;
-    lists.push(list);
-    saveAndRender();
-  });
-
-  tasksContainer.addEventListener('click', (e) => {
-    if (e.target.tagName.toLowerCase() === 'input') {
-      const selectedList = lists.find((list) => list.id === selectedListId);
-      const selectedTask = selectedList.tasks.find((task) => task.id === e.target.id);
-      selectedTask.complete = e.target.checked;
-      save();
-      renderTaskCount(selectedList);
-    }
-  });
-
-
-  newTaskForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const taskName = newTaskInput.value;
-    if (taskName == null || taskName === '') return;
-    const task = createTask(taskName);
-    newTaskInput.value = null;
-    const selectedList = lists.find((list) => list.id === selectedListId);
-    selectedList.tasks.push(task);
-    saveAndRender();
-  });
-
 const todoList = () => {
   const selectedList = lists.find((list) => list.id === selectedListId);
   console.log(selectedList);
@@ -93,5 +37,64 @@ const todoList = () => {
   renderLists(lists, listsContainer, selectedListId);
   display(selectedList, selectedListId);
 };
+
+const saveAndRender = () => {
+  save();
+  todoList();
+};
+
+listsContainer.addEventListener('click', (e) => {
+  if (e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+});
+
+clearCompleteTasksButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  selectedList.tasks = selectedList.tasks.filter((task) => !task.complete);
+  saveAndRender();
+});
+
+deleteListButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  lists = lists.filter((list) => list.id !== selectedListId);
+  selectedListId = null;
+  saveAndRender();
+});
+
+newListForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const listName = newListInput.value;
+  if (listName == null || listName === '') return;
+  const list = createList(listName);
+  newListInput.value = null;
+  lists.push(list);
+  saveAndRender();
+});
+
+tasksContainer.addEventListener('click', (e) => {
+  if (e.target.tagName.toLowerCase() === 'input') {
+    const selectedList = lists.find((list) => list.id === selectedListId);
+    const selectedTask = selectedList.tasks.find((task) => task.id === e.target.id);
+    selectedTask.complete = e.target.checked;
+    save();
+    renderTaskCount(selectedList);
+  }
+});
+
+
+newTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const taskName = newTaskInput.value;
+  if (taskName == null || taskName === '') return;
+  const task = createTask(taskName);
+  newTaskInput.value = null;
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  selectedList.tasks.push(task);
+  saveAndRender();
+});
+
 
 todoList();
